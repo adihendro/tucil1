4485e7d95616b6aab8ba3c0d1f75c338f498052c
adihendro <adi21hendro@gmail.com>
2021-02-05 00:28:12 +0700

    create openFile checker

--- gui.py
+++ gui.py
@@ -13,7 +13,10 @@ def compute():
         return
 
     # Get text and key
-    text = ent_text.get().upper()
+    if(file_content == ''):
+        text = ent_text.get().upper()
+    else:
+        text = file_content
     key = ent_key.get().upper()
     # Get mode
     mode = var1.get()
@@ -28,10 +31,11 @@ def compute():
     
 
 def openFile(): 
-    file = askopenfile(mode ='rb') 
-    if file is not None: 
-        content = file.read() 
-        print(content)
+    f = askopenfile(mode ='rb') 
+    if f is not None: 
+        file_content = f.read() 
+
+file_content = ''
 
 
 window = Tk()
@@ -44,12 +48,12 @@ frm_form = Frame(relief=RIDGE, borderwidth=3)
 frm_form.pack()
 
 lbl_text = Label(master=frm_form, text='Enter message:')
-ent_text = Entry(master=frm_form, width=40)
+ent_text = Entry(master=frm_form, width=30)
 lbl_text.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 ent_text.grid(row=0, column=1, padx=5, pady=5)
 
 lbl_key = Label(master=frm_form, text='Enter key:')
-ent_key = Entry(master=frm_form, width=40)
+ent_key = Entry(master=frm_form, width=30)
 lbl_key.grid(row=1, column=0, padx=5, pady=5, sticky='w')
 ent_key.grid(row=1, column=1, padx=5, pady=5)
 
@@ -59,10 +63,9 @@ btn_open.grid(row=2, column=1, padx=5, pady=5, sticky='w')
 var1 = StringVar()
 var2 = StringVar()
 var3 = StringVar()
-var1.set(0)
-var2.set(0)
-var3.set(0)
-
+var1.set(1)
+var2.set(1)
+var3.set(1)
 
 lbl_mode = Label(master=frm_form, text='Choose mode:')
 lbl_mode.grid(row=3, column=0, padx=5, pady=5, sticky="w")
@@ -95,7 +98,7 @@ btn_compute = Button(master=frm_form, text='Go!', width=10, command=compute)
 btn_compute.grid(row=13, column=1, padx=5, pady=5, sticky='w')
 
 lbl_result = Label(master=frm_form, text='Result:')
-lbl_result_text = Label(master=frm_form, text='R')
+lbl_result_text = Label(master=frm_form, text='Click "Go!" to see magic')
 lbl_result.grid(row=14, column=0, padx=5, pady=5, sticky="w")
 lbl_result_text.grid(row=14, column=1, padx=5, pady=5, sticky="w")
 
